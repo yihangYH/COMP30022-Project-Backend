e6cb4df4487c52019c902077f7d3692fba3b4799
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-09-06 18:52:03 +0800

    init the project

---
+++ IT/src/test/java/IT/Project/IT/ItApplicationTests.java
@@ -0,0 +1,13 @@
+package IT.Project.IT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

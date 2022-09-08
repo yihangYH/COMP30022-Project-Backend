a3b3f1e91bc3255cd46813e02814ec8178a32e1a
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-09-08 20:08:45 +0800

    create response class for response return

---
+++ IT/src/main/java/IT/Project/IT/Response.java
@@ -0,0 +1,15 @@
+package IT.Project.IT;
+
+public class Response {
+
+    private String status;
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}

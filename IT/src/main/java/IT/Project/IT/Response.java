a88d155509bcb3507cb464884fcf0e767e7f0608
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-09-10 11:49:20 +0800

    update response class, so response will return useful id

--- IT/src/main/java/IT/Project/IT/Response.java
+++ IT/src/main/java/IT/Project/IT/Response.java
@@ -4,11 +4,21 @@ public class Response {
 
     private String status;
 
+    private String id;
+
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
